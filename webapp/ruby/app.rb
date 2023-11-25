410b75b1de96a3414f809e3291f82025ef3cd9fd
shinter61 <shintaro-matsumoto@c-fo.com>
2023-11-25 14:28:13 +0900

    fix

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -540,12 +540,11 @@ module Isupipe
         livestream_ids = query_result.map { |livecomment_model| livecomment_model.fetch(:livestream_id) }.uniq
 
         tags_array = {}
-        tx.xquery("
-          SELECT tags.id AS id, tags.name AS name, livestream_tags.livestream_id AS livestream_id
+        tag_query = "SELECT tags.id AS id, tags.name AS name, livestream_tags.livestream_id AS livestream_id
           FROM tags 
           INNER JOIN livestream_tags ON tags.id = livestream_tags.tag_id 
-          WHERE livestream_tags.livestream_id IN (#{ livestream_ids.map { |_row| '?' }.join(',') })", 
-        *livestream_ids).map do |tag_model|
+          WHERE livestream_tags.livestream_id IN (#{ livestream_ids.map { |_row| '?' }.join(',') })"
+        tx.xquery(tag_query, livestream_ids).map do |tag_model|
           tags_array[tag_model.fetch(:livestream_id)] ||= []
           tags_array[tag_model.fetch(:livestream_id)] << {
             id: tag_model.fetch(:id),
